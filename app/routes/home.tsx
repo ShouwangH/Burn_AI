c0ad3df8fa337a056acce4a687f2aca5c241166f
Shouwang Huang <shouwangh82@gmail.com>
2025-09-29 17:58:39 -0400

    start styling landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import { authClient } from "~/src/lib/auth-client";
 import SignIn from "./signin";
 import SignUp from "./signup";
 import SignInG from "./signingoogle";
+import { useState } from "react";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -12,13 +13,30 @@ export function meta({ }: Route.MetaArgs) {
 
 export default function Home() {
   const { data, isPending, error } = authClient.useSession()
+  const logOut  = async () =>{ await authClient.signOut()}
+  const [issignin, setissignin] = useState(true)
   if (data) {
-    return <div>Hello, {data.user.email}!</div>
+    return (<>
+    <div>Hello, {data.user.email}!</div> 
+    <button onClick={logOut}>Log out</button>
+    </>
+    )
   } else {
-    return <div>
+    return <div className="flex justify-evenly items-center">
+      <div className="text-9xl  ">
+        AAAAHHHH
+      </div>
+      <div className="">
+      {(issignin) ? 
+      <div>
       <SignIn />
       <SignInG />
+      </div> :
+      <div>
       <SignUp />
+      </div> }
+      <button></button>
+    </div>
     </div>
   }
 }
